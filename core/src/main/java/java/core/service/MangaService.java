3096e778fb8967d6d19fe2de6f4d7fbdf6a4f82c
PierreSanchez <pierre.sanchez@hei.yncrea.fr>
2021-10-17 12:27:27 +0200

    UPDATE <core.service> implemented manga and review services

--- core/src/main/java/java/core/service/MangaService.java
+++ core/src/main/java/java/core/service/MangaService.java
@@ -1,4 +1,34 @@
 package java.core.service;
 
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.core.dao.MangaDAO;
+import java.core.entity.Manga;
+
+@Service
+@Transactional
 public class MangaService {
+
+    private MangaDAO mangaDAO;
+
+    public MangaService (MangaDAO _mangaDao){
+        this.mangaDAO = _mangaDao;
+    }
+
+    public void delete(Manga manga){
+        mangaDAO.delete(manga);
+    }
+
+    public void delete(long id){
+        mangaDAO.deleteById(id);
+    }
+
+    public Manga getManga(long id){
+        return mangaDAO.getOne(id);
+    }
+
+    public void addManga(Manga manga){
+        mangaDAO.save(manga);
+    }
 }
