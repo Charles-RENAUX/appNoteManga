3efc44cd0479c9438bce94bbf94f23ff2793818c
PierreSanchez <pierre.sanchez@hei.yncrea.fr>
2021-10-17 11:01:15 +0200

    UPDATE <web> update dependencies, setup initializer and webCOnfig

--- web/src/main/java/java/web/Initilizer.java
+++
@@ -1,4 +0,0 @@
-package java.web;
-
-public class Initilizer {
-}
--- web/src/main/java/java/web/config/WebConfig.java
+++ web/src/main/java/java/web/config/WebConfig.java
@@ -1,4 +1,35 @@
 package java.web.config;
 
-public class WebConfig {
+import com.shieldsolutions.velocity.view.VelocityConfigurer;
+import com.shieldsolutions.velocity.view.VelocityViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "java.web.controller")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+    }
+
+    @Bean
+    public VelocityConfigurer velocityConfigurer(){
+        VelocityConfigurer velConf = new VelocityConfigurer();
+        velConf.setResourceLoaderPath("/WEB-INF/velocity");
+        return velConf;
+    }
+
+    @Bean
+    public VelocityViewResolver velocityViewResolver(){
+        VelocityViewResolver velVRes = new VelocityViewResolver();
+        velVRes.setSuffix(".vm");
+        return velVRes;
+    }
 }
